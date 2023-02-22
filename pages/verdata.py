bafc628dd73bbd7ae15397ae333291fb97783cea
pjluispb <124328519+pjluispb@users.noreply.github.com>
2023-02-22 17:57:35 -0400

    Update verdata.py

--- pages/verdata.py
+++ pages/verdata.py
@@ -4,9 +4,8 @@ import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 
 
-#----newurl = 'https://raw.githubusercontent.com/pjluispb/miscvs/main/Prondanmin23.csv'
-newurl = pd.read_csv("Prondanmin23.csv", index_col='cedula')
-#st.write(newurl.head(5))
+urlcsv = 'https://raw.githubusercontent.com/pjluispb/miscvs/main/Prondanmin23.csv'
+newurl = pd.read_csv(urlcsv, index_col='cedula')
 
 
 with st.expander('ver data'):
@@ -14,7 +13,3 @@ with st.expander('ver data'):
 regresar = st.button('Volver')
 if regresar:
     switch_page('logmi')
-
-#busq = pdwks.where(pdwks['Cédula de Identidad']='5125570')
-#busq = pdwks[pdwks['Cédula de Identidad'].str.contains('5125570')]
-#fibusq
