5711dae7a990506a658fc964a5a4c01f44f71965
pjluispb <124328519+pjluispb@users.noreply.github.com>
2023-03-08 12:22:32 -0400

    Update reiniciar.py

--- pages/reiniciar.py
+++ pages/reiniciar.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 
+logina = st.session_state['logina']
+
 #st.sidebar_state['collapsed']
 switch_page('askCed-deta')
