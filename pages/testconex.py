c6a28662185b7313ed9d384676ee1acbc8d913b6
pjluispb <124328519+pjluispb@users.noreply.github.com>
2023-03-17 09:31:41 -0400

    Update testconex.py

--- pages/testconex.py
+++ pages/testconex.py
@@ -19,5 +19,8 @@ res=accesos.fetch()
    
 st.write('gc = ',gc)
 st.write('deta = ',deta)
-#gsheetsTit = gc.spreadsheet_titles()       #obtiene la lista de todas las gsheets
+try:
+   gsheetsTit = gc.spreadsheet_titles()       #obtiene la lista de todas las gsheets
+except:
+   st.write('No funciona gc.spreadsheet_titles()')
 #files = [x for x in gsheetsTit if x.startswith('M2')]    #obtiene la lista de los gsheets que comienzan con M2
