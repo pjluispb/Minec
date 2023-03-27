2db141cc4792dac077d9ce6c73d6ae186a876342
pjluispb <124328519+pjluispb@users.noreply.github.com>
2023-03-27 05:59:34 -0400

    Update verdata.py

--- pages/verdata.py
+++ pages/verdata.py
@@ -8,10 +8,14 @@ from streamlit_echarts import st_echarts
 from PIL import Image
 
 def row_style(row):
-    if row['paycon'] == 'SI':
+    if row['paycon'] == 'SI++':
+        return pd.Series('background-color: #7986cb; color:#000000', row.index)
+    elif row['paycon'] == 'PENDIENTE x DIFERENCIA':
+        return pd.Series('background-color: #ff6f00; color:#000000', row.index)
+    elif row['paycon'] == 'SI':
         return pd.Series('background-color: #8ede99; color:#000000', row.index)
     elif row['paycon'] == 'PENDIENTE':
-        return pd.Series('background-color: #fdd834; color:#000229', row.index)
+        return pd.Series('background-color: #fdd834; color:#000000', row.index)
     else:
         return pd.Series('', row.index)
 
@@ -140,7 +144,8 @@ else:
             st_echarts(options=options, height = '250px')
             st.write('---')
     
-with st.expander('ver data'):
+with st.expander('Data del distrito ' + dtto):
+    df = df.reindex(columns=['Distrito', 'Categoria', 'cedula', 'Nombres', 'Apellidos', 'paycon', 'Modalidad', 'MontoApagar', 'montoPago', 'Diferencia', 'fuenteOrigen', 'referenciaPago', 'fechaPago', 'Email', 'Telefono'])
     st.dataframe(df.style.apply(row_style, axis=1))
     
 regresar = st.button('Volver')
