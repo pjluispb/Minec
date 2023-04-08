662cb11262b8b55b313b4f978e08b2a558685da7
pjluispb <124328519+pjluispb@users.noreply.github.com>
2023-04-08 07:02:48 -0400

    Update matchpayuser.py

--- pages/matchpayuser.py
+++ pages/matchpayuser.py
@@ -119,7 +119,7 @@ for index, row in dfpendientes.iterrows():
             #st.write('REFERENCIA : ',list(drefbus['REFERENCIA'].items())[0][1], ' - INGRESO : ',list(drefbus['INGRESO'].items())[0][1], ' - FECHA : ',list(drefbus['FECHA'].items())[0][1], ' - DESCRIPCION : ', list(drefbus['DESCRIPCION'].items())[0][1])
             #st.write('ReferenciaPago: ',row['referenciaPago'], row['Apellidos'], row['Nombres'],' Categoria: ', row['Categoria'],' ID : ', row['key'],' MontoPago : ', row['montoPago'], 'MontoApagar : ',row['MontoApagar'], row['paycon'])
             #st.write('eanumber : ', eanumber(row['montoPago'])[0])
-            diferencia = round(float(row['MontoApagar'])-abs(eanumber(row['montoPago'])[0]))
+            diferencia = round(float(row['MontoApagar'])-abs(eanumber(str(row['montoPago']))[0]))
             #st.write('Diferencia = ', diferencia)
             if abs(diferencia)<=int(margenp): vpayc = 'SI'
             else:
