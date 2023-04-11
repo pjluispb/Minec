2eeabc7dff90057b9cbb4b23078cf0be34574fbb
pjluispb <124328519+pjluispb@users.noreply.github.com>
2023-04-11 16:07:22 -0400

    Update updauser.py

--- pages/updauser.py
+++ pages/updauser.py
@@ -136,7 +136,7 @@ if b0:
                                                     if first['fuenteOrigen']=='Pago Movil': indexrFO=0
                                                     else: indexrFO=1
                                                 except:
-                                                    indexF0=0
+                                                    indexrF0=0
                                                 fuenteOrigen = ph1.radio('Origen del pago(Transferencia o Pago Movil) : ', options=['Pago Movil', 'Transferencia'],horizontal=True, index=indexrFO)
                                                 if fuenteOrigen != '': sherr = True
                                         else: fuenteOrigen = ph1.text_input(label='Origen del pago', value=first['fuenteOrigen'],disabled=True)
