10bd691eeb000e57500a5c4faf8f8070d84d1c56
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-03-27 09:31:28 -0400

    Add files via upload

--- logmi.py
+++ logmi.py
@@ -1,26 +1,25 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
-# Aplicacion base...en pagesMinec....significa que va para la raiz y el resto en el subdir pages
 from deta import Deta
 from PIL import Image
+
+deta = Deta(st.secrets["deta_key"])
+accesos = deta.Base('minec-accesos')
+res=accesos.fetch()
+#res.items
 
 st.set_page_config(
     page_title="Minec Reg App",
     page_icon="🧊",
     layout="wide",
     initial_sidebar_state="collapsed",
-
 )
 
-deta = Deta(st.secrets["deta_key"])
-accesos = deta.Base('minec-accesos')
-res=accesos.fetch()
 
 imagen1 = Image.open('minecLogo.jpeg')
 imagen2 = Image.open('minecLogoTitle.jpeg')
 st.image(imagen1)
 st.image(imagen2)
-
 st.subheader('Login')
 with st.form('Login Minec'):
     usuario = st.text_input('Usuario', placeholder='nombre de usuario')
@@ -30,21 +29,21 @@ with st.form('Login Minec'):
         buser = [x for x in res.items if x['user']==usuario]
         if len(buser)>0:
             bclave = buser[0]['clave']
-            #bclave
-            if str(bclave)==str(clave):
-                logina = buser[0]
-                st.session_state['logina'] = logina
-                st.write(logina)
-                if buser[0]['tipou']=='AdminFinanzas':
-                    switch_page('checkpay')
-                else:
-                    switch_page('BienvenidaU')
-                
+            print(bclave)
+            if str(bclave).startswith('vov4-'):
+                if str(bclave)==str(clave):
+                    logina = buser[0]
+                    st.session_state['logina'] = logina
+                    st.write(logina)
+                    if buser[0]['tipou']=='AdminFinanzas':
+                        switch_page('checkpay')
+                    else:
+                        switch_page('BienvenidaU2024')
+                else: st.write('Clave Inválida')
             else:
-                st.write('Clave Inválida')
+                st.toast('Clave Inválida')
                 #st.write('Clave Invalida', clave, type(clave), bclave, type(bclave))
         else:
-            st.write('**** Datos Invalidos **** ')
-            st.write(' ⚠️  Intenta nuevamente  ')
-    
+            st.write('Nombre de Usuario Invalido')
+
     
